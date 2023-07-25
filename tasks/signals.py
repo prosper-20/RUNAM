6c9bb2646275e6c991e40fe145799b5a6431638d
prosper <edwardprosper001@gmail.com>
2023-07-25 09:17:11 +0100

    updates

--- tasks/signals.py
+++ tasks/signals.py
@@ -1,7 +1,8 @@
 from django.db.models.signals import post_save
-from .models import Shop, ShopProfile
+from .models import Shop, ShopProfile, Task
 from django.dispatch import receiver
-
+from django.conf import settings
+from django.core.mail import send_mail
 
 
 @receiver(post_save, sender=Shop)
@@ -13,5 +14,29 @@ def create_shop_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=Shop)
 def save_shop_profile(sender, instance, **kwargs):
     instance.shopprofile.save()
+
+
+@receiver(post_save, sender=Task)
+def send_broadcast_mail(sender, instance, created, **kwargs):
+    if created:
+        subject = ' New Task Alert!!'
+        message = f'A new task has been created, be the first to view it'
+        from_email = settings.DEFAULT_FROM_EMAIL
+        to_email = [instance.email]
+        send_mail(subject, message, from_email, to_email, fail_silently=True)
+
+
+
+
+
+# @receiver(post_save, sender=CustomUser)
+# def send_activation_email(sender, instance, created, **kwargs):
+#     if created:
+#         subject = 'Welcome to our website'
+#         message = f'Hi {instance.profile.username}, welcome to our website!'
+#         from_email = settings.DEFAULT_FROM_EMAIL
+#         to_email = [instance.email]
+#         send_mail(subject, message, from_email, to_email, fail_silently=True)
+        
 
 
