4f51c3099169377bccce83fdefbeda455112e00b
prosper <edwardprosper001@gmail.com>
2023-11-23 11:54:03 +0100

    'new'

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
+# import requests
+
+# response = requests.get("https://emailvalidation.abstractapi.com/v1/?api_key=68cfcf7e3e554734a0364618fa606998&email=edwardprosper00@gmail.com")
+# print(response.status_code)
+# print(response.content)
+
+
+
 import requests
 
-response = requests.get("https://emailvalidation.abstractapi.com/v1/?api_key=68cfcf7e3e554734a0364618fa606998&email=edwardprosper001@gmail.com")
+response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=c2069a02bcba4f23984a9cf56677cb40")
 print(response.status_code)
 print(response.content)
