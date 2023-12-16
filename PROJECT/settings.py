6a29ba8b170876c25582a515353fa3b98002a1df
prosper <edwardprosper001@gmail.com>
2023-12-16 10:06:27 +0100

    'updates'

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'runam.up.railway.app']
 
 
 
